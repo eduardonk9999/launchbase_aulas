96a757dcd524d8517337585a034693556c8d7200
Eduardo <thiago.novak7@gmail.com>
2020-09-15 23:08:45 -0300

    Add, conteudo dinamico

---
+++ back-end/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "HN1UjzRSdBk",
+        title: "CSS GRID",
+        duration: "36 min",
+        price: "Free"
+    },
+    {
+        id: "BaI8dHUthLA",
+        title: "Dê super poderes ao CSS com SASS",
+        duration: "01h 06min",
+        price: "Free"
+    },
+    {
+        id: "DsbeclaivTo",
+        title: "PR 17",
+        duration: "21 min",
+        price: "Free"
+    },
+    {
+        id: "zy-B9d2ZVrw",
+        title: "Navegador Flutuante com FT. ELECTRONJS",
+        duration: "22 min",
+        price: "Free"
+    },
+    {
+        id: "IPLTUlRFxgo",
+        title: "Nenhuma PR passará sem padrôes",
+        duration: "46 min",
+        price: "Free"
+    },
+    {
+        id: "GSqR2i-Pq6o",
+        title: "Desvendando a variável THIS",
+        duration: "48 min",
+        price: "Free"
+    }
+]
--- back-end/server.js
+++ back-end/server.js
@@ -3,6 +3,8 @@ const nunjucks = require('nunjucks')
 // Iniciando o Server
 const server = express()
 
+// Arq de vídeos
+const videos = require("./data")
 
 server.use(express.static('public'))
 
@@ -15,12 +17,27 @@ nunjucks.configure("views", {
 
 
 server.get("/", function(req, res){
-    return res.render("about")
+    const data = {
+        avatar_url: "https://avatars1.githubusercontent.com/u/18013936?s=460&u=6473577c965c80a490813f368c029bf7cbb6aa85&v=4",
+        name: "Eduardo Silva",
+        role: "Front End",
+        description: 'Fazedor de site',
+        links: [
+            { name: "Github", url:""},
+            { name: "Twitter", url:""},
+            { name: "Linkedin", url:""},
+
+        ]
+    }
+
+
+
+    return res.render("about", { data: data })
 })
 
 
 server.get("/classes", function(req, res){
-    return res.render("classes")
+    return res.render("classes", { items: videos })
 })
 
 
